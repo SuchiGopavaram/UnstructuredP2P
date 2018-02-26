7a72740f570fed3ba0078573cd2e5a36a8b0723e
SuchiGopavaram <gopavaramsuchindramani@gmail.com>
2018-02-26 12:28:18 -0700

    Added Features: peerlisten.java file now has added rcv() and send() methods. Also sending unstructuredpeer.java file, where no changes where made compared to the last push.

--- peerListen.java
+++ peerListen.java
@@ -5,24 +5,42 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class peerListen extends Thread{
+	static DatagramSocket Sock;
+	
+	public peerListen() {
+		
+	}
 	
 	public void run() {
-		
+		while(true) {
+			try {
+				String rcvReq = rcv();
+			} catch (IOException e) {
+				System.err.println("IOException Occured.");
+				System.exit(1);
+			}
+			
+		}
 	}
 	
-	public static void main(String[] args) {
-		
-	}
-	
-	public static void rcvSnd(String Message, String ip, int Port) throws IOException {
-		DatagramSocket Sock = new DatagramSocket();
-		InetAddress IP = InetAddress.getByName(ip);
+	public static String rcv() throws IOException {
+		Sock = new DatagramSocket();		
 		byte[] rcv = new byte[1023];
 		DatagramPacket rcvpkt = new DatagramPacket(rcv, rcv.length);
 		Sock.receive(rcvpkt);
-		byte[] send = new byte[1023];
+		String reply = new String(rcvpkt.getData(),0,rcvpkt.getLength());
+		System.out.println(reply);
+		Sock.close();
+		return reply;
+	}
+	
+	public static void send(String Message, String ip, int Port) throws IOException {
+		Sock = new DatagramSocket();
+		InetAddress IP = InetAddress.getByName(ip);
+		byte[] send = Message.getBytes();
 		DatagramPacket sndpkt = new DatagramPacket(send, send.length, IP, Port);
 		Sock.send(sndpkt);
 	}
